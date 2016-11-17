a3e2f5d90ab7e7edcaf033de7ea966525a987d26
vagrant <vagrant@machine1.puphpet>
2016-11-17 10:59:14 +0000

    new page

--- index.php
+++ index.php
@@ -4,14 +4,20 @@
 <head>
   <meta charset="utf-8">
 
-    <title>matthieu.sarran.fr</title>
-    <meta name="description" content="matthieu.sarran.fr">
-    <meta name="author" content="matthieu.sarran.fr">
+  <title>matthieu.sarran.fr</title>
+  <meta name="description" content="matthieu.sarran.fr">
+  <meta name="author" content="matthieu.sarran.fr">
 
-    <link rel="stylesheet" href="styles.css">
-
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,500" rel="stylesheet">
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-
+  <div class="overlay"></div>
+  <div class="content">
+    <img class="avatar" src="images/profil.jpg" />
+    <h1>Matthieu Sarran</h1>
+    <h2>Pianist. Foot-Fan. Mountainbiker. Lyonnais.</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+  </div>
 </body>
 </html>
