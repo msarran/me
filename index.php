dfbaf2c0fa955ab39a02d2c4cf7b5e5afe9e93d5
msarran <matthieu.sarran@gmail.com>
2016-11-17 10:25:00 +0100

    installed backround image (#1)

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<h1>Hello, World!</h1>
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+    <title>matthieu.sarran.fr</title>
+    <meta name="description" content="matthieu.sarran.fr">
+    <meta name="author" content="matthieu.sarran.fr">
+
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+<body>
+
+</body>
+</html>
