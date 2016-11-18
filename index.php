65f805dff888f5c35b8c23352bc9b708d3a9c36e
vagrant <vagrant@machine1.puphpet>
2016-11-18 16:03:58 +0000

    update on matthieu.sarran.fr

--- index.php
+++ index.php
@@ -7,19 +7,31 @@
   <title>matthieu.sarran.fr</title>
   <meta name="description" content="matthieu.sarran.fr">
   <meta name="author" content="matthieu.sarran.fr">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,500" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,500"
+        rel="stylesheet">
   <link rel="stylesheet" href="styles.css">
 </head>
 <body>
   <div class="overlay"></div>
+  <div class="header">
+     <a href="https://play.spotify.com/user/matthieu.sarran">
+      <img class="logo logo-spotify" src="http://www.clker.com/cliparts/5/c/a/f/141490005811489798026274.spotify-logo-horizontal-white-rgb-hi.png" />
+     </a>
+     <a href="https://plus.google.com/100986751108406410140">
+      <img class="logo logo-googleplus" src="http://www.fluxarray.com/googleplus.png" />
+     </a>
+  </div>
   <div class="content">
-    <img class="avatar" src="images/profil.jpg" />
+    <img class="avatar" src="images/profil.jpg"/>
     <h1>Matthieu Sarran</h1>
     <h2>Christian. Pianist. Mountainbiker. Soccer fan.</h2>
-    <p>Living in Lyon, France and a fan of "l'Olympique Lyonnais", I was born on August 13th. 2002 and I am in 9th grade in
-    "Centre Scolaire de Saint-Martin en Haut". I'm French and American and also Christian. Passions: Music - Soccer - Cinema -
-    Christ.</p>
+    <p>Living in Lyon, France and a fan of "l'Olympique Lyonnais",
+       I'm 14 years old and I am in 9th grade in
+       "Centre Scolaire de Saint-Martin en Haut".
+       I'm French and American and also Christian.
+       Passions: Music - Soccer - Cinema - Christ.</p>
   </div>
 </body>
 </html>
